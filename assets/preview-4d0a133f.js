eb6fd0b6916f4d10a04b7865c746f4e792de192f
shenxiang11 <shenxiang11@users.noreply.github.com>
2023-05-30 03:10:56 +0000

    deploy: 3976c546d06e7e7a902d9984a6fbc0097af46d79

---
+++ assets/preview-4d0a133f.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-76a85963.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-fbfdd147.js"),["./DocsRenderer-EYKKDMVH-fbfdd147.js","./iframe-76a85963.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./react-18-f58e807b.js","./index-80ae7d84.js","./index-b9cfa47a.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-4d0a133f.js.map
